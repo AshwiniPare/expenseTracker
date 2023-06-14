0cd6787306fab8d484ba8629f8c7978873b7f9ba
AshwiniPare <ashwinipbhatt@gmail.com>
2023-06-14 09:51:17 -0700

    setting up for live

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker', process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
     dialect: 'mysql',
-     host: 'localhost'
+     host: process.env.DB_HOST
     });
 
     module.exports = sequelize;
